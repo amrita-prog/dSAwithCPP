375982d6c14d79fa4e97d96ef37998df67587a92
Amrita Mishra <itizamritamishra@gmail.com>
2025-09-11 20:40:38 +0530

    update

--- string/Q2.cpp
+++ string/Q2.cpp
@@ -1,30 +1,47 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-void isPalindrome(string str) {
-  
-  	// Stores the reverse of the string s
+void isPalindrome(string str){
     string rev = str;
-
-    // Reverse the string
-    reverse(rev.begin(), rev.end());
-
-    // If rev is equal to str
-    if (str == rev)
-        cout << "\"" << str
-      	<< "\" is palindrome." << endl;
-      
-	// If rev is NOT equal to str
-    else
-        cout << "\"" << str
-      	<< "\" is NOT palindrome." << endl;
+    reverse(str.begin(),str.end());
+    if(str == rev){
+      cout<<str <<" is palindrome"<<endl;
+    }else{
+      cout<<str <<" is not a palindrome"<<endl;
+    }
 }
 
-int main() {
+int main(){
+  string str;
+
+  cout<<"Enter a string : ";
+  cin>>str;
+
+  getline(cin,str);
+  isPalindrome(str);
+  
+  return 0;
+
+}
     
-  	string str;
-    cout << "Enter a string: ";
-    getline(cin, str);
-    isPalindrome(str);
-    return 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
