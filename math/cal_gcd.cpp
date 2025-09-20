0dc9f808ba5f2b10cc2d496a65f88877c083bbe2
Amrita Mishra <itizamritamishra@gmail.com>
2025-09-20 19:12:39 +0530

    update

---
+++ math/cal_gcd.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;    
+
+int gcd(int a, int b){
+    while(a > 0 && b > 0){
+        if(a > b){
+            a = a % b;
+        }else{
+            b = b % a;
+        }
+    }
+    if(a == 0){
+        return b;
+    }else{
+        return a;
+    }
+}
+
+int main(){
+    int a, b;
+    cout<<"Enter Two Number : ";
+    cin>>a>>b;
+
+    cout<<gcd(a, b)<<endl;
+    return 0;
+}
+
