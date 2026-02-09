ef2d3c0894eb4fb9fc95f744ff949542068bb23c
Amrita Mishra <itizamritamishra@gmail.com>
2026-02-09 20:11:04 +0530

    only one subset

--- recursion/subsetSum.cpp
+++ recursion/subsetSum.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-void subsetSum(int i, vector<int> &ds, int s, int sum, int arr[], int n) {
+bool subsetSum(int i, vector<int> &ds, int s, int sum, int arr[], int n) {
     if(i == n){
         if(s == sum){
             for(auto value: ds){
@@ -33,3 +33,47 @@ int main(){
 
     return 0;
 }
+
+
+
+
+
+// print only one subset that adds up to the given sum:
+
+// bool subsetSum(int i, vector<int> &ds, int s, int sum, int arr[], int n) {
+//     if(i == n){
+//         if(s == sum){
+//             for(auto value: ds){
+//                 cout<<value<<" ";
+//             }
+//             cout<<endl;
+//             return true;
+//         }
+//         return false;
+//     }
+
+//     ds.push_back(arr[i]);
+//     s += arr[i];
+
+//     if(subsetSum(i+1, ds, s, sum, arr, n) == true){
+//         return true;
+//     }
+
+//     s -= arr[i];
+//     ds.pop_back();
+
+//     if(subsetSum(i+1, ds, s, sum, arr, n) == true){
+//         return true;
+//     }
+//     return false;
+// }
+
+// int main(){
+//     int arr[] = {1,2,1};
+//     int n = 3;
+//     int sum = 2;
+//     vector<int> ds;
+//     subsetSum(0, ds, 0, sum, arr, n);
+
+//     return 0;
+// }
